ca7f0ebd625932d116c79dceceb22f5f9f1fcaf5
Saksham Gupta <guptasaksham82@gmail.com>
2023-02-21 00:47:06 +0530

    cleaning

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/Error");
 
-const {needValidationForUser: needValidation} = require("../middleware/middleware");
+const { needValidationForUser: needValidation } = require("../middleware/middleware");
 
 const router = express.Router();
 
@@ -21,8 +21,10 @@ router.post("/register", needValidation, async (req, res) => {
 			username: req.body.username,
 		});
 		const d = await User.register(user, req.body.password);
-		req.flash("success", "User is Registered");
-		res.redirect("/campgrounds");
+		req.login(d, (err) => {
+			req.flash("success", "User is Registered");
+			res.redirect("/campgrounds");
+		});
 	} catch (e) {
 		req.flash("error", e.message);
 		res.redirect("/user/register");
@@ -34,20 +36,20 @@ router.get("/login", (req, res) => {
 	res.render("./users/login");
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect: "/user/login", failureFlash: true,keepSessionInfo: true}), (req, res) => {
-		req.flash("success","Welcome Back");
-		const redirectPath = req.session.returnTo || '/campgrounds';
-		res.redirect(redirectPath);
-	}
+router.post("/login", passport.authenticate("local", { failureRedirect: "/user/login", failureFlash: true, keepSessionInfo: true }), (req, res) => {
+	req.flash("success", "Welcome Back");
+	const redirectPath = req.session.returnTo || '/campgrounds';
+	res.redirect(redirectPath);
+}
 );
 
-router.get("/logout",(req,res)=>{
-	req.logout((err)=>{
-		if(err) console.log(err)
-		req.flash("success","Logged Out Successfully");
-		res.redirect("/user/login")
+router.get("/logout", (req, res) => {
+	req.logout((err) => {
+		if (err) console.log(err);
+		req.flash("success", "Logged Out Successfully");
+		res.redirect("/user/login");
 	});
 
-})
+});
 
 module.exports = router;
